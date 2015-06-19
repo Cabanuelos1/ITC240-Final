d7b9e9b331e3c1ff3dbdd7d693d311d20ae2429e
Cabanuelos1 <cabanuelos1@crimson.ua.edu>
2015-06-18 19:51:07 -0700

    final project

---
+++ final/workoutsdata.sql
@@ -0,0 +1,11 @@
+INSERT INTO `Workouts` (`Exercise`, `Caloriesburned`) VALUES
+('Swimming',	'720 per hour'),
+('Weight lifting',	'490 per hour'),
+('Stair climber',	'765 per hour'),
+('Rowing machine',	'572 per hour'),
+('Stretching',	'260 per hour'),
+('Running',	'654 per hour'),
+('Basketball',	'675 per hour'),
+('Jumping rope',	'817 per hour'),
+('Soccer',	'890 per hour'),
+('Walking',	'270 per hour');
