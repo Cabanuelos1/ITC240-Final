f8651ecca4c2a9061e574a17f437f48b3fc1cf4e
Cabanuelos1 <cabanuelos1@crimson.ua.edu>
2015-06-18 19:50:33 -0700

    final project

---
+++ final/mealsdata.sql
@@ -0,0 +1,11 @@
+INSERT INTO `Meals` (`Dish`, `Calories`) VALUES
+('Stuffed pepper',	'110'),
+('Baked green beans',	'124'),
+('Shrimp stir fry',	'339'),
+('Turkey Wraps',	'245'),
+('Spinach quesadillas',	'141'),
+('Fruit salad',	'129'),
+('Chicken kabobs',	'183'),
+('Loaded baked potato soup',	'323'),
+('Chicken Nuggets',	'315'),
+('Crab salad',	'120');
